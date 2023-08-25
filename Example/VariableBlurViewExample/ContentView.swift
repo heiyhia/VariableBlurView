33c7340ea81b748b2299622442515296b967bb91
herh <herh@yimidida.com>
2023-08-25 18:00:53 +0800

    init

---
+++ Example/VariableBlurViewExample/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  VariableBlurViewExample
+//
+//  Created by A. Zheng (github.com/aheze) on 5/30/23.
+//  Copyright © 2023 A. Zheng. All rights reserved.
+//
+
+import SwiftUI
+import VariableBlurView
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            ScrollView(.vertical) {
+                VStack {
+                    Image("Sky")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 6, style: .continuous))
+                        .padding()
+                }
+                .padding(.top, 50)
+            }
+
+            VStack {
+                VariableBlurView()
+                    .frame(height: 200)
+                    .allowsHitTesting(false)
+
+                Spacer()
+            }
+        }
+        .ignoresSafeArea(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
